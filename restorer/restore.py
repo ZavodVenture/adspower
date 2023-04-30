8e377fd1d2e6ce223df46d214c2e4decdab157b5
Zhora1142 <avdonin.egor@gmail.com>
2023-04-30 18:25:13 +0300

    loading fix

--- restorer/restore.py
+++ restorer/restore.py
@@ -65,6 +65,17 @@ def worker(ws_index, metamask_index):
 
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//button[@data-testid="create-password-import"]'))).click()
 
+    driver.implicitly_wait(5)
+
+    while 1:
+        try:
+            driver.find_element(By.XPATH, '//div[@class="loading-overlay"]')
+        except:
+            break
+        else:
+            sleep(1)
+            continue
+
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//button[@data-testid="onboarding-complete-done"]'))).click()
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//button[@data-testid="pin-extension-next"]'))).click()
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//button[@data-testid="pin-extension-done"]'))).click()
